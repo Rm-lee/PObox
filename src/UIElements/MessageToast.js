e823b142605be79298a7374146c0fdf494b12a0b
roger-lee <rmlee0003@gmail.com>
2020-04-11 19:19:20 -0500

    changed message style,added timed close on message

--- src/UIElements/MessageToast.js
+++ src/UIElements/MessageToast.js
@@ -5,11 +5,24 @@ function MessageToast(props) {
   const handleDismiss = () => {
     props.close(false);
   };
+  useEffect(() => {
+    if (props.show) {
+      setTimeout(() => {
+        props.close(false);
+      }, 2500);
+    }
+  }, [props.show]);
   return (
     <>
       {props.show ? (
         <Message
-          style={{ position: "absolute", top: 100 }}
+          style={{
+            position: "absolute",
+            top: 100,
+            backgroundColor: "#333",
+            width: "280px",
+            color: "white"
+          }}
           onDismiss={handleDismiss}
           header={props.messageHeader}
           content={props.messageContent}
